36ce52192ccad81e943766f039b627577d7123bb
Crowdbotics <team@crowdbotics.com>
2019-04-19 19:48:42 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.liftr_2459.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "heroku" {
+  version = "~> 1.5"
+}
+provider "cloudflare" {
+  version = "~> 1.9"
+}
+terraform {
+  backend "s3" {
+    bucket  = "crowdbotics-terraform"
+    key     = "liftr-2459"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
